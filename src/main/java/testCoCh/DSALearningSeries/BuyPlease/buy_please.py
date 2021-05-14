4f873b7fb2889896f13a2204f7bd74ccea5ffee0
Alexander <apetrovskiy@hotmail.com>
2021-05-14 18:36:02 +0300

    Update buy_please.py

--- src/main/java/testCoCh/DSALearningSeries/BuyPlease/buy_please.py
+++ src/main/java/testCoCh/DSALearningSeries/BuyPlease/buy_please.py
@@ -1,2 +1,9 @@
 def buy_please(a: int, b: int, x: int, y: int) -> int:
     return a*x+b*y
+
+
+if __name__ == '__main__':
+    input = input().split(" ")
+    a, b, x, y = map(int, input)
+    print(buy_please(int(input[0]), int(
+        input[1]), int(input[2]), int(input[3])))
